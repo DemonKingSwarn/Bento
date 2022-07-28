3edb61f855719eb833dec34ff5a6707529910a76
justmessingaround <69480361+DemonKingSwarn@users.noreply.github.com>
2022-07-28 21:23:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,19 +14,13 @@ const CONFIG = {
   name: 'DemonKingSwarn',
   imageBackground: false,
   openInNewTab: true,
-  twelveHourFormat: false,
+  twelveHourFormat: true,
 
   // Greetings
   greetingMorning: 'Good morning!',
   greetingAfternoon: 'Good afternoon,',
   greetingEvening: 'Good evening,',
   greetingNight: 'Go to Sleep!',
-
-  // Weather
-  weatherKey: 'dff56702403345df82065430222701',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
   // defaultLatitude: '37.775',
